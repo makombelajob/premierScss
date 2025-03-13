12f7d3117201dfa4b1476aa9bd3b19742d59ddba
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-13 14:31:40 +0100

    App finished

--- js/script-2.js
+++ js/script-2.js
@@ -7,6 +7,8 @@ const template = document.querySelector("#template");
 for(const task of fromSession) {
     const init = template.content.cloneNode(true);
 
+    const ul = init.querySelector("ul");
+    ul.style.listStyle = "none";
     const check = init.querySelector("#checkbox");
     const label = init.querySelector("label");
     label.textContent = task;
